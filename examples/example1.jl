ff7267eb9c4491032c1d8cd0a87329cb0a0d80e2
Matthew Fishman <mfishman@flatironinstitute.org>
2021-04-13 10:08:04 -0400

    Add initial definition of Observer type and example of usage

---
+++ examples/example1.jl
@@ -0,0 +1,33 @@
+using Observer
+
+# Series for π/4
+f(k) = (-1)^(k+1)/(2k-1)
+
+function my_iterative_function(niter; observer!, observe_step)
+  π_approx = 0.0
+  for n in 1:niter
+    π_approx += f(n)
+    if iszero(n % observe_step)
+      update!(observer!; π_approx = 4π_approx, iteration = n)
+    end
+  end
+  return 4π_approx
+end
+
+# Measure the relative error from π at each iteration
+err_from_π(; π_approx, kwargs...) = abs(π - π_approx) / π
+
+# Record which iteration we are at
+iteration(; iteration, kwargs...) = iteration
+obs = Observers(["Error" => err_from_π, "Iteration" => iteration])
+
+niter = 10000
+π_approx = my_iterative_function(niter; observer! = obs, observe_step = 1000)
+
+@show π_approx
+@show obs
+@show func(obs, "Iteration")
+@show func(obs, "Error")
+@show results(obs, "Iteration")
+@show results(obs, "Error")
+
--- src/Observer.jl
+++ src/Observer.jl
@@ -1,5 +1,34 @@
 module Observer
 
-using JLD
+export Observers, func, results, update!
+
+# TODO: Rename Observer
+# TODO: allow optionally specifying the element type of the results
+# if they are known ahead of time.
+struct Observers <: AbstractDict{String, Pair{Function, Vector{Any}}}
+  data::Dict{String, Pair{Function, Vector{Any}}}
+  function Observers(kv)
+    d = Dict{String, Pair{Function, Vector{Any}}}()
+    for (k, v) in kv
+      d[k] = v => Any[]
+    end
+    return new(d)
+  end
+end
+
+Base.getindex(obs::Observers, n) = obs.data[n]
+Base.length(obs::Observers) = length(obs.data)
+Base.iterate(obs::Observers, args...) = iterate(obs.data, args...)
+
+func(obs::Observers, n) = first(obs[n])
+results(obs::Observers, n) = last(obs[n])
+
+function update!(obs::Observers, args...; kwargs...)
+  for (k, v) in obs
+    func_k, results_k = obs[k]
+    push!(results_k, func_k(args...; kwargs...))
+  end
+  return obs
+end
 
 end # module
