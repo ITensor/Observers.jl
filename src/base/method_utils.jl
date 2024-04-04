bf31421b50ac891f7fcf48754b469362556774ab
mtfishman <mfishman@flatironinstitute.org>
2024-04-04 12:04:07 -0400

    Fix issue with keyword varargs

--- src/base/method_utils.jl
+++ src/base/method_utils.jl
@@ -18,11 +18,10 @@ end
 function remove_unsupported_kwargs(method::Method, kwargs)
   # Extract the keywords of the keyword arguments declared in the definition of method
   method_keywords = Base.kwarg_decl(method)
-  # Remove any keyword argument varargs in the method definition.
-  filter!(
-    x -> !(length(string(x)) ≥ 4 && string(x)[(end - 2):end] == "..."), method_keywords
-  )
-  # Attach the arguments of the input keyword arguments for the keywords
-  # defined with the method.
-  return method_keywords .=> [kwargs[keyword] for keyword in method_keywords]
+  has_varargs = any(endswith("...") ∘ string, method_keywords)
+  filtered_kwargs = kwargs
+  if !has_varargs
+    filtered_kwargs = pairs(Dict([keyword => kwargs[keyword] for keyword in method_keywords]))
+  end
+  return filtered_kwargs
 end
