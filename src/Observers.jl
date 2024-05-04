5027cd6014e4bed89c0fed56d8c11f8ef53aab54
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-04 18:05:50 -0400

    Generalize constructors (#22)

--- src/Observers.jl
+++ src/Observers.jl
@@ -2,11 +2,10 @@ module Observers
 export observer, update!, get_function, set_function!, insert_function!
 # Deprecated
 export results
-include("base/method_utils.jl")
-include("abstractdataframe/column_functions.jl")
-include("abstractdataframe/deprecated.jl")
-include("dataframe/observer.jl")
-include("dataframe/deprecated.jl")
+include("method_utils.jl")
+include("column_functions.jl")
+include("observer.jl")
+include("deprecated.jl")
 # For backwards compatibility since some
 # libraries were using `Observers.DataFrames`.
 using DataFrames: DataFrames
--- src/observer.jl
+++ src/observer.jl
@@ -5,7 +5,7 @@ using DataFrames: DataFrame
 
 const ColumnName = Union{Symbol,String}
 
-function observer(names::Vector{<:ColumnName}, functions::Vector{<:Function}; kwargs...)
+function observer(names, functions; kwargs...)
   df = DataFrame(map(name -> name => Union{}[], names); kwargs...)
   for (name, func) in zip(names, functions)
     set_function!(df, name, func)
@@ -13,9 +13,7 @@ function observer(names::Vector{<:ColumnName}, functions::Vector{<:Function}; kw
   return df
 end
 
-function observer(
-  names::Tuple{Vararg{ColumnName}}, functions::Tuple{Vararg{Function}}; kwargs...
-)
+function observer(names::Tuple, functions::Tuple; kwargs...)
   return observer(collect(names), collect(functions); kwargs...)
 end
 
@@ -23,11 +21,11 @@ end
 # Default to empty columns with element type `Union{}`
 # so they get automatically promoted to the first type that gets pushed
 # into them.
-function observer(name_function_pairs::Vector{<:Pair{<:ColumnName,<:Function}}; kwargs...)
+function observer(name_function_pairs::Vector{<:Pair}; kwargs...)
   return observer(first.(name_function_pairs), last.(name_function_pairs); kwargs...)
 end
 
-function observer(name_function_pairs::Pair{<:ColumnName,<:Function}...; kwargs...)
+function observer(name_function_pairs::Pair...; kwargs...)
   return observer(first.(name_function_pairs), last.(name_function_pairs); kwargs...)
 end
 
@@ -41,12 +39,12 @@ end
 
 observer(; kwargs...) = observer(NamedTuple(kwargs))
 
-function observer(functions::Vector{<:Function}; kwargs...)
-  return observer(string.(functions), functions; kwargs...)
-end
-
 function observer(::Tuple{}; kwargs...)
   return observer((), (); kwargs...)
+end
+
+function observer(functions::Vector{<:Function}; kwargs...)
+  return observer(string.(functions), functions; kwargs...)
 end
 
 function observer(functions::Tuple{Vararg{Function}}; kwargs...)
